adb1cd93084d8a7b658d8f75617a4b99f4a2c6e9
Beas <89128576+Beas-github@users.noreply.github.com>
2025-03-04 19:48:11 +0900

    Add files via upload
    v1.31

--- T2/init_cfg.sh
+++ T2/init_cfg.sh
@@ -1,29 +1,24 @@
 #!/usr/bin/env bash
 
 echo ">>>> Initial Config Start <<<<"
-echo "[TASK 1] Setting Root Password"
+echo "[TASK 1] Setting SSH with Root"
 printf "qwe123\nqwe123\n" | passwd >/dev/null 2>&1
-
-echo "[TASK 2] Setting Sshd Config"
-sed -i "s/^PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
 sed -i "s/^#PermitRootLogin prohibit-password/PermitRootLogin yes/g" /etc/ssh/sshd_config
-systemctl restart sshd
+sed -i "s/^PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config.d/60-cloudimg-settings.conf
+systemctl restart sshd  >/dev/null 2>&1
 
-echo "[TASK 3] Change Timezone & Setting Profile & Bashrc"
-# Change Timezone
-ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtime
-
-#  Setting Profile & Bashrc
+echo "[TASK 2] Profile & Bashrc & Change Timezone"
 echo 'alias vi=vim' >> /etc/profile
 echo "sudo su -" >> .bashrc
+ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtimew
 
-echo "[TASK 4] Disable ufw & AppArmor"
+echo "[TASK 3] Disable ufw & AppArmor"
 systemctl stop ufw && systemctl disable ufw >/dev/null 2>&1
 systemctl stop apparmor && systemctl disable apparmor >/dev/null 2>&1
 
-echo "[TASK 5] Install Packages"
+echo "[TASK 4] Install Packages"
 apt update -qq >/dev/null 2>&1
-apt-get install prettyping sshpass bridge-utils net-tools jq tree resolvconf wireguard ngrep ipset iputils-arping ipvsadm vim unzip -y -qq >/dev/null 2>&1
+apt-get install prettyping sshpass bridge-utils net-tools jq tree resolvconf wireguard ngrep ipset iputils-arping ipvsadm vim unzip conntrack -y -qq >/dev/null 2>&1
 # Install Batcat - https://github.com/sharkdp/bat
 apt-get install bat -y >/dev/null 2>&1
 echo 'alias cat=batcat' >> /etc/profile
@@ -35,61 +30,51 @@ wget https://github.com/andreazorzetto/yh/releases/download/v0.4.0/yh-linux-amd6
 unzip yh-linux-amd64.zip
 mv yh /usr/local/bin/
 
-echo "[TASK 6] Change DNS Server IP Address"
+echo "[TASK 5] Change DNS Server IP Address"
 echo -e "nameserver 8.8.8.8" > /etc/resolvconf/resolv.conf.d/head
 resolvconf -u
 
-echo "[TASK 7] Setting Local DNS Using Hosts file"
+echo "[TASK 6] Setting Local DNS Using Hosts file"
 echo "192.168.56.200 k8s-m" >> /etc/hosts
 for (( i=1; i<=$1; i++  )); do echo "192.168.56.20$i k8s-w$i" >> /etc/hosts; done
 
-echo "[TASK 8] Install containerd.io"
-# Install Runtime - Containerd https://kubernetes.io/docs/setup/production-environment/container-runtimes/
-cat <<EOF > /etc/modules-load.d/containerd.conf
-overlay
-br_netfilter
-EOF
-modprobe overlay
+echo "[TASK 7] Install containerd.io"
+# packets traversing the bridge are processed by iptables for filtering
+echo 1 > /proc/sys/net/ipv4/ip_forward
+# enable br_filter for iptables 
 modprobe br_netfilter
-sysctl --system
-
-cat <<EOF > /etc/sysctl.d/kubernetes.conf
-net.bridge.bridge-nf-call-iptables  = 1
-net.ipv4.ip_forward                 = 1
-net.bridge.bridge-nf-call-ip6tables = 1
-EOF
-sysctl -p >/dev/null 2>&1
-sysctl --system >/dev/null 2>&1
-
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-chmod a+r /etc/apt/keyrings/docker.asc
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
 apt-get update >/dev/null 2>&1
 apt-get install containerd.io -y >/dev/null 2>&1
 mkdir -p /etc/containerd
 containerd config default > /etc/containerd/config.toml
 
-echo "[TASK 9] Using the systemd cgroup driver"
-#sed -i'' -r -e "/runc.options/a\            SystemdCgroup = true" /etc/containerd/config.toml
+echo "[TASK 8] Using the systemd cgroup driver"
 sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
-systemctl restart containerd
 
-echo "[TASK 10] Disable and turn off SWAP"
+# avoid WARN&ERRO(default endpoints) when crictl run  
+cat <<EOF > /etc/crictl.yaml
+runtime-endpoint: unix:///run/containerd/containerd.sock
+image-endpoint: unix:///run/containerd/containerd.sock
+EOF
+
+systemctl restart containerd && systemctl enable containerd
+systemctl enable --now kubelet
+
+echo "[TASK 9] Disable and turn off SWAP"
 swapoff -a
 
-echo "[TASK 11] Install Kubernetes components (kubeadm, kubelet and kubectl) - v$2"
-#curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg >/dev/null 2>&1
-#echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.27/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.27/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
-apt-get update >/dev/null 2>&1
-apt-get install -y kubelet kubectl kubeadm >/dev/null 2>&1
-apt-mark hold kubelet kubeadm kubectl >/dev/null 2>&1
+echo "[TASK 10] Install Kubernetes components (kubeadm, kubelet and kubectl) - v$2"
+# add kubernetes repo
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v$2/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v$2/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+# add docker-ce repo with containerd
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc >/dev/null 2>&1
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update the apt package index, install kubelet, kubeadm and kubectl, and pin their version
+apt update -qq >/dev/null 2>&1
+apt-get install -y kubelet kubectl kubeadm >/dev/null 2>&1 && apt-mark hold kubelet kubeadm kubectl >/dev/null 2>&1
 systemctl enable kubelet && systemctl start kubelet
 
 echo ">>>> Initial Config End <<<<"
